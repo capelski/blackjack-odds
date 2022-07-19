1eb2928c3c93c8f7de00629f806b1c8b46a0e65d
Carles Capellas <carles@peekvision.org>
2022-07-19 21:18:45 +0200

    Remove irrelevant decksNumber constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
 export const dealerStandThreshold = 17;
 export const decimalsNumber = 2;
-export const decksNumber = 6;
 export const maximumScore = 21;
--- src/logic/outcomes-set.ts
+++ src/logic/outcomes-set.ts
@@ -1,4 +1,3 @@
-import { decksNumber } from '../constants';
 import { ScoreKey } from '../models';
 import { OutcomesSet } from '../types';
 
@@ -6,19 +5,18 @@ import { OutcomesSet } from '../types';
  * Returns the set of outcomes available (i.e. cards and weights)
  */
 export const getOutcomesSet = (): OutcomesSet => {
-    const outcomesNumber = 4 * decksNumber;
     const allOutcomes = [
-        { key: ScoreKey.hard2, symbol: '2', values: [2], weight: outcomesNumber },
-        { key: ScoreKey.hard3, symbol: '3', values: [3], weight: outcomesNumber },
-        { key: ScoreKey.hard4, symbol: '4', values: [4], weight: outcomesNumber },
-        { key: ScoreKey.hard5, symbol: '5', values: [5], weight: outcomesNumber },
-        { key: ScoreKey.hard6, symbol: '6', values: [6], weight: outcomesNumber },
-        { key: ScoreKey.hard7, symbol: '7', values: [7], weight: outcomesNumber },
-        { key: ScoreKey.hard8, symbol: '8', values: [8], weight: outcomesNumber },
-        { key: ScoreKey.hard9, symbol: '9', values: [9], weight: outcomesNumber },
+        { key: ScoreKey.hard2, symbol: '2', values: [2], weight: 1 },
+        { key: ScoreKey.hard3, symbol: '3', values: [3], weight: 1 },
+        { key: ScoreKey.hard4, symbol: '4', values: [4], weight: 1 },
+        { key: ScoreKey.hard5, symbol: '5', values: [5], weight: 1 },
+        { key: ScoreKey.hard6, symbol: '6', values: [6], weight: 1 },
+        { key: ScoreKey.hard7, symbol: '7', values: [7], weight: 1 },
+        { key: ScoreKey.hard8, symbol: '8', values: [8], weight: 1 },
+        { key: ScoreKey.hard9, symbol: '9', values: [9], weight: 1 },
         // There four 10-valued outcomes: 10, J, Q, K
-        { key: ScoreKey.hard10, symbol: '10', values: [10], weight: 4 * outcomesNumber },
-        { key: ScoreKey.soft11, symbol: 'A', values: [1, 11], weight: outcomesNumber }
+        { key: ScoreKey.hard10, symbol: '10', values: [10], weight: 4 },
+        { key: ScoreKey.soft11, symbol: 'A', values: [1, 11], weight: 1 }
     ];
     const totalWeight = allOutcomes.reduce((reduced, next) => reduced + next.weight, 0);
 
