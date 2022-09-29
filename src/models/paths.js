e43c8a3ed35faa8c24328074c1bf0e2d228a4727
Carles Capellas <carles@peekvision.org>
2022-09-29 13:07:02 +0200

    feat: introduce routes pre-rendering

---
+++ src/models/paths.js
@@ -0,0 +1,11 @@
+// Intentional Javascript file, for use during webpack builds
+
+module.exports = {
+    Paths: {
+        casinoRules: '/casino-rules',
+        dealerCards: '/dealer-cards',
+        legend: '/legend',
+        playerDecisions: '/player-decisions',
+        playerScores: '/player-scores'
+    }
+};
--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,6 +1,15 @@
+const PrerenderSPAPlugin = require('@dreysolano/prerender-spa-plugin');
+const path = require('path');
 const { merge } = require('webpack-merge');
 const baseConfig = require('./base.config');
+const { Paths } = require('../src/models/paths.js');
 
 module.exports = merge(baseConfig, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new PrerenderSPAPlugin({
+            routes: Object.values(Paths),
+            staticDir: path.join(__dirname, '..', 'docs')
+        })
+    ]
 });
