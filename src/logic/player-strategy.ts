0a72e565864d227769fb9e89a12aad8a7fb7481f
Carles Capellas <carles@peekvision.org>
2023-02-05 20:42:06 +0100

    Improve player strategies descriptions

--- src/logic/player-strategy.ts
+++ src/logic/player-strategy.ts
@@ -1,32 +1,23 @@
-import { probabilityLabels } from '../constants';
 import { PlayerStrategy, PlayerDecision } from '../models';
 import { Dictionary } from '../types';
 import { getDisplayPlayerDecision } from './player-decision';
 
 export const playerStrategyLegend: Dictionary<string, PlayerStrategy> = {
-    [PlayerStrategy.standThreshold]: `${PlayerDecision.hit} only when score is LOWER than `,
-    [PlayerStrategy.hitBusting_standLessThanDealer]: `${PlayerDecision.hit} only when [ ${probabilityLabels.playerBusting} ] for ${PlayerDecision.hit} is LOWER than [ ${probabilityLabels.playerLessThanDealer} ] for ${PlayerDecision.stand}`,
-    [PlayerStrategy.hitBustingOrLower_standLessThanDealer]: `${PlayerDecision.hit} only when [ ${
-        probabilityLabels.playerBusting
-    } + ${probabilityLabels.playerLessThanCurrent('X')} ] for ${
+    [PlayerStrategy.standThreshold]: '-',
+    [PlayerStrategy.hitBusting_standLessThanDealer]: `Lower probability: Score lower than dealer by ${PlayerDecision.stand}, Busting by ${PlayerDecision.hit}`,
+    [PlayerStrategy.hitBustingOrLower_standLessThanDealer]: `Lower probability: Score lower than dealer by ${PlayerDecision.stand}, Busting + Score lower than current by ${PlayerDecision.hit}`,
+    [PlayerStrategy.minimumLoss_hit_stand]: `Minimize loss: ${PlayerDecision.hit}, ${PlayerDecision.stand}`,
+    [PlayerStrategy.maximumWin_hit_stand]: `Maximize win: ${PlayerDecision.hit}, ${PlayerDecision.stand}`,
+    [PlayerStrategy.maximumPayout_hit_stand]: `Maximize payout: ${PlayerDecision.hit}, ${PlayerDecision.stand}`,
+    [PlayerStrategy.maximumPayout_hit_stand_double]: `Maximize payout: ${PlayerDecision.hit}, ${
+        PlayerDecision.stand
+    }, ${getDisplayPlayerDecision(PlayerDecision.doubleHit)}`,
+    [PlayerStrategy.maximumPayout_hit_stand_split]: `Maximize payout: ${PlayerDecision.hit}, ${
+        PlayerDecision.stand
+    }, ${getDisplayPlayerDecision(PlayerDecision.splitHit)}`,
+    [PlayerStrategy.maximumPayout_hit_stand_double_split]: `Maximize payout: ${
         PlayerDecision.hit
-    } is LOWER than [ ${probabilityLabels.playerLessThanDealer} ] for ${PlayerDecision.stand}`,
-    [PlayerStrategy.minimumLoss_hit_stand]: `Minimum ${probabilityLabels.playerLoss}: ${PlayerDecision.hit} / ${PlayerDecision.stand}`,
-    [PlayerStrategy.maximumWin_hit_stand]: `Maximum ${probabilityLabels.playerWin}: ${PlayerDecision.hit} / ${PlayerDecision.stand}`,
-    [PlayerStrategy.maximumPayout_hit_stand]: `Maximum ${probabilityLabels.playerAdvantagePayout}: ${PlayerDecision.hit} / ${PlayerDecision.stand}`,
-    [PlayerStrategy.maximumPayout_hit_stand_double]: `Maximum ${
-        probabilityLabels.playerAdvantagePayout
-    }: ${PlayerDecision.hit} / ${PlayerDecision.stand} / ${getDisplayPlayerDecision(
+    }, ${PlayerDecision.stand}, ${getDisplayPlayerDecision(
         PlayerDecision.doubleHit
-    )}`,
-    [PlayerStrategy.maximumPayout_hit_stand_split]: `Maximum ${
-        probabilityLabels.playerAdvantagePayout
-    }: ${PlayerDecision.hit} / ${PlayerDecision.stand} / ${getDisplayPlayerDecision(
-        PlayerDecision.splitHit
-    )}`,
-    [PlayerStrategy.maximumPayout_hit_stand_double_split]: `Maximum ${
-        probabilityLabels.playerAdvantagePayout
-    }: ${PlayerDecision.hit} / ${PlayerDecision.stand} / ${getDisplayPlayerDecision(
-        PlayerDecision.doubleHit
-    )} / ${getDisplayPlayerDecision(PlayerDecision.splitHit)}`
+    )}, ${getDisplayPlayerDecision(PlayerDecision.splitHit)}`
 };
