ea306f248b8a602f9959c6933c0f1592ad1b08c0
Carles Capellas <carles@peekvision.org>
2023-02-08 11:33:14 +0100

    Move pre-rendering routes logic to TS

--- src/logic/paths.ts
+++ src/logic/paths.ts
@@ -1,5 +1,21 @@
 import { Paths, ScoreKey } from '../models';
+import { Dictionary } from '../types';
 
 export const getScorePlayerDecisionPath = (scoreKey: ScoreKey) => {
     return Paths.scorePlayerDecisions.replace(':scoreKey', scoreKey);
 };
+
+const prerenderingRoutesDictionary: Dictionary<string[], Paths> = {
+    [Paths.dealerCards]: [Paths.dealerCards],
+    [Paths.legend]: [Paths.legend],
+    [Paths.playerDecisions]: [Paths.playerDecisions],
+    [Paths.scorePlayerDecisions]: Object.values(ScoreKey).map((scoreKey) => {
+        return Paths.scorePlayerDecisions.replace(':scoreKey', scoreKey);
+    }),
+    [Paths.strategyAndRules]: [Paths.strategyAndRules]
+};
+
+export const prerenderingRoutes = Object.values(prerenderingRoutesDictionary).reduce(
+    (reduced, next) => reduced.concat(next),
+    []
+);
--- webpack/production.config.js
+++ webpack/production.config.js
@@ -2,25 +2,14 @@ const PrerenderSPAPlugin = require('@dreysolano/prerender-spa-plugin');
 const path = require('path');
 const { merge } = require('webpack-merge');
 const baseConfig = require('./base.config');
-// The following files are generated via npm script from the corresponding TS version
-const { Paths } = require('../src/models/webpack-paths');
-const { ScoreKey } = require('../src/models/webpack-score-key');
-
-const routes = [
-    Paths.dealerCards,
-    Paths.legend,
-    Paths.playerDecisions,
-    Paths.strategyAndRules,
-    ...Object.values(ScoreKey).map((scoreKey) => {
-        return Paths.scorePlayerDecisions.replace(':scoreKey', scoreKey);
-    })
-];
+// The following file is generated via npm script
+const { prerenderingRoutes } = require('./routes/logic/paths');
 
 module.exports = merge(baseConfig, {
     mode: 'production',
     plugins: [
         new PrerenderSPAPlugin({
-            routes,
+            routes: prerenderingRoutes,
             staticDir: path.join(__dirname, '..', 'docs')
         })
     ]
