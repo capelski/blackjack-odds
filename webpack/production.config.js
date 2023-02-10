3d853296852736401eb1125c580f72a982fd9d72
Carles Capellas <carles@peekvision.org>
2023-02-10 18:22:18 +0100

    Add pre-rendering logging

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -5,11 +5,17 @@ const baseConfig = require('./base.config');
 // The following file is generated via npm script
 const { prerenderingRoutes } = require('./routes/logic/paths');
 
+console.log(`Prerendering ${prerenderingRoutes.length} routes...`);
+
 module.exports = merge(baseConfig, {
     mode: 'production',
     plugins: [
         new PrerenderSPAPlugin({
             routes: prerenderingRoutes,
+            postProcess: (renderedRoute) => {
+                console.log(`√ ${renderedRoute.route}`);
+                return renderedRoute;
+            },
             staticDir: path.join(__dirname, '..', 'docs')
         })
     ]
