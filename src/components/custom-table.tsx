77df8e9fdfac712f0b06fd2885ffc8168c80782b
Carles Capellas <carles@peekvision.org>
2023-02-14 11:48:13 +0100

    Refactor custom-table rendering

--- src/components/custom-table.tsx
+++ src/components/custom-table.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, useMemo } from 'react';
 import { Cell, Column, Row, TableInstance, TableOptions, useTable } from 'react-table';
 
 export type CustomCell<TData extends {}, TColumn extends Column<TData>> = Omit<
@@ -65,55 +65,71 @@ export const CustomTable = <TData extends {}, TColumn extends Column<TData>>(
         data: props.data
     });
 
+    const { headers, data } = useMemo(() => {
+        const headers = headerGroups.map((headerGroup) => {
+            return {
+                cells: headerGroup.headers.map((column) => {
+                    const headerProps = column.getHeaderProps();
+                    return (
+                        <th
+                            {...column.getHeaderProps()}
+                            style={{
+                                ...headerProps.style,
+                                ...baseCellProps,
+                                fontWeight: 'bold',
+                                padding: '2px 4px'
+                            }}
+                            key={column.id}
+                        >
+                            {column.render('Header')}
+                        </th>
+                    );
+                }),
+                rowProps: headerGroup.getHeaderGroupProps()
+            };
+        });
+
+        const data = rows.map((row) => {
+            prepareRow(row);
+            return {
+                cells: row.cells.map((cell) => {
+                    const cellProps = cell.getCellProps();
+                    const columnStyle: CSSProperties = {
+                        ...cellProps.style,
+                        ...baseCellProps,
+                        padding: 0,
+                        ...(props.cellStyle ? props.cellStyle(cell) : {})
+                    };
+
+                    return (
+                        <td {...cellProps} style={columnStyle} key={row.id + '-' + cell.column.id}>
+                            {cell.render('Cell')}
+                        </td>
+                    );
+                }),
+                rowProps: row.getRowProps()
+            };
+        });
+
+        return { headers, data };
+    }, [headerGroups, rows]);
+
     return (
         <table width={props.width} {...getTableProps()}>
-            <thead>
-                {headerGroups.map((headerGroup, headerIndex) => (
-                    <tr {...headerGroup.getHeaderGroupProps()} key={`header-row-${headerIndex}`}>
-                        {headerGroup.headers.map((column) => {
-                            const headerProps = column.getHeaderProps();
-                            return (
-                                <th
-                                    {...column.getHeaderProps()}
-                                    style={{
-                                        ...headerProps.style,
-                                        ...baseCellProps,
-                                        fontWeight: 'bold',
-                                        padding: '2px 4px'
-                                    }}
-                                    key={column.id}
-                                >
-                                    {column.render('Header')}
-                                </th>
-                            );
-                        })}
-                    </tr>
-                ))}
-            </thead>
+            {headers.length > 0 && (
+                <thead>
+                    {headers.map((headerGroup, headerIndex) => (
+                        <tr {...headerGroup.rowProps} key={`header-row-${headerIndex}`}>
+                            {headerGroup.cells}
+                        </tr>
+                    ))}
+                </thead>
+            )}
             <tbody {...getTableBodyProps()}>
-                {rows.map((row) => {
-                    prepareRow(row);
+                {data.map((row, rowIndex) => {
                     return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map((cell) => {
-                                const cellProps = cell.getCellProps();
-                                const columnStyle: CSSProperties = {
-                                    ...cellProps.style,
-                                    ...baseCellProps,
-                                    padding: 0,
-                                    ...(props.cellStyle ? props.cellStyle(cell) : {})
-                                };
-
-                                return (
-                                    <td
-                                        {...cellProps}
-                                        style={columnStyle}
-                                        key={row.id + '-' + cell.column.id}
-                                    >
-                                        {cell.render('Cell')}
-                                    </td>
-                                );
-                            })}
+                        <tr {...row.rowProps} key={`row-${rowIndex}`}>
+                            {row.cells}
                         </tr>
                     );
                 })}
